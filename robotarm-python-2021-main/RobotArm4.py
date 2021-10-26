644d338b84c6e55e358fae25963de54837ef8294
Lennert <99069053@mydavinci.nl>
2021-10-26 09:25:58 +0200

    RobotArm

---
+++ robotarm-python-2021-main/RobotArm4.py
@@ -0,0 +1,28 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 4')
+
+robotArm.speed = 2
+
+# Jouw python instructies zet je vanaf hier:
+
+for i in range(2):
+    robotArm.grab()
+    for i in range(2):
+        robotArm.moveRight()
+    robotArm.drop()
+    for i in range(2):
+        robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+for i in range(2):
+    robotArm.moveRight()
+    robotArm.grab()
+    robotArm.moveLeft()
+    robotArm.drop()
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
